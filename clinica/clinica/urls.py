ddd55fe682e337611373f987a3c79bd9b445071a
AlissonAl72 <alissonmt2010@gmail.com>
2025-07-15 23:44:00 -0400

    Correção de dados
    Algumas correções basicas

--- clinica/clinica/urls.py
+++ clinica/clinica/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     
     path('', include('core.urls')),
 
-    # As URLs de autenticação que você já tinha.
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
